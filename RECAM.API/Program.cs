2beadf30238828c664a386235354277eaed5375e
JxCl-L <jiaxili0624@gmail.com>
2026-08-14 19:15:28 +1000

    chore: register Swagger UI in development environment (#1)

--- RECAM.API/Program.cs
+++ RECAM.API/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer(); // 让 Swagger 能扫到 endpoint
+builder.Services.AddSwaggerGen(); // 注册 Swagger Generator 生成器
+
 // BUILDER => APP
 
 var app = builder.Build();
@@ -15,10 +18,11 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>(); // after Build, before other Use
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-
-// }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();         // 提供 /swagger/v1/swagger.json
+    app.UseSwaggerUI();       // 提供 /swagger 网页
+}
 
 app.UseHttpsRedirection();
 
