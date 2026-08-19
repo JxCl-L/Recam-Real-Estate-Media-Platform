e47b8df7db7d5cb1f37cc6dd745aab707f45994f
JxCl-L <jiaxili0624@gmail.com>
2026-08-19 21:24:41 +1000

    feat: add EF Core migration for initial schema with User cascade delete fixes

--- RECAM.DataAccess/Data/RECAMDbContext.cs
+++ RECAM.DataAccess/Data/RECAMDbContext.cs
@@ -24,18 +24,28 @@ public class RECAMDbContext : IdentityDbContext<ApplicationUser>
     {
         base.OnModelCreating(builder); // must 1st line to get all DbSet ready
 
-        // add 1:1 relationship FK Agent:User
+        // add 1:1 relationship FK User → XX & remove all cascade delete (soft delete IsDeleted)
         builder.Entity<Agent>()
         .HasOne(a => a.User) // 1 agent has 1 user
         .WithOne() // user side not add navigation to agent
-        .HasForeignKey<Agent>(a => a.Id); // FK is agent.Id, that points to user and == UserId
-        // add 1:1 FK PhotographyCompany:User
-        builder.Entity<PhotographyCompany>().HasOne(p => p.User).WithOne().HasForeignKey<PhotographyCompany>(p => p.Id);
+        .HasForeignKey<Agent>(a => a.Id) // FK is agent.Id, that points to user and == UserId
+        .OnDelete(DeleteBehavior.NoAction); // user delete => not casecade delete agent
+
+        builder.Entity<PhotographyCompany>().HasOne(p => p.User).WithOne().HasForeignKey<PhotographyCompany>(p => p.Id).OnDelete(DeleteBehavior.NoAction);
+
+        builder.Entity<ListingCase>().HasOne(l => l.User).WithMany().HasForeignKey(l => l.UserId).OnDelete(DeleteBehavior.NoAction);
+
+        builder.Entity<MediaAsset>().HasOne(m => m.User).WithMany().HasForeignKey(m => m.UserId).OnDelete(DeleteBehavior.NoAction);
 
         // global filter soft deleted
         builder.Entity<ListingCase>().HasQueryFilter(l => !l.IsDeleted);
         builder.Entity<MediaAsset>().HasQueryFilter(m => !m.IsDeleted);
         builder.Entity<ApplicationUser>().HasQueryFilter(u => !u.IsDeleted);
+
+        builder.Entity<Agent>().HasQueryFilter(a => !a.User.IsDeleted);
+        builder.Entity<PhotographyCompany>().HasQueryFilter(p => !p.User.IsDeleted);
+
+        builder.Entity<CaseContact>().HasQueryFilter(c => !c.ListingCase.IsDeleted);
     }
 
     private void ApplyAuditTimestamps()
